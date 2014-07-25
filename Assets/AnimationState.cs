b5ca560f002b5a61c37ca7beccaa45a1b644fb6c
Snutt <anders@art-studio.se>
2014-07-25 03:16:13 +0200

    Text shows up after the animation

---
+++ Assets/AnimationState.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationState : MonoBehaviour 
+{
+	private Animator _animator;
+    private Action _callbackAction;
+	private bool _stateReset;
+
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+	}
+
+	public void SetTrigger(string trigger, Action callback = null)
+	{
+		_animator.SetTrigger(trigger);
+		_callbackAction = callback;
+	}
+
+	protected void Update()
+	{
+		if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f && !_stateReset)
+			_stateReset = true;
+
+		if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f && _stateReset)
+		{
+			_stateReset = false;
+
+			if (_callbackAction != null)
+			{
+				_callbackAction();
+				_callbackAction = null;
+			}
+		}
+	}
+}
---
+++ Assets/MinimizeAtAwake.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimizeAtAwake : MonoBehaviour 
+{
+
+	void Awake () 
+	{
+		transform.localScale = new Vector3(0, 0, 0);
+	}
+}
